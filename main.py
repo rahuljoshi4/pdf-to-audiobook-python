66dc7c34afb1e12fd93fa9e9556968c2b533b448
Rahul Dev Joshi <z5592088@ad.unsw.edu.au>
2026-02-01 16:15:45 +1100

    added customisation on volume, rate and gender

--- main.py
+++ main.py
@@ -6,9 +6,24 @@ book = askopenfilename()
 pdfReader = PyPDF2.PdfReader(book)
 pages = len(pdfReader.pages)
 
+voice = pyttsx3.init()
+
+volume = int(input("Set a volume from 0 to 10: "))
+voice.setProperty('volume', volume/10)
+
+rate = input("Set speaking rate from 0 to 100: ")
+voice.setProperty('rate', rate)
+
+gender = input("Male or Female speaking voice?: ")
+
+if gender.lower() == "male":
+    voice.setProperty("voice", 1)
+else:
+    voice.setProperty("voice", 0)
+
+
 for num in range(0, pages):
     page = pdfReader.pages[num]
     text = page.extract_text()
-    player = pyttsx3.init()
-    player.say(text)
-    player.runAndWait()
+    voice.say(text)
+    voice.runAndWait()
